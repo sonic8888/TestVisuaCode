cd58f15ad5a5f9b904ceda4e46f773a857ecd6f2
sonic <sonic45600@gmail.com>
2025-09-05 01:24:05 +0300

    изменил SqliteDb

--- TestVisualCode/Idb.cs
+++ TestVisualCode/Idb.cs
@@ -5,9 +5,9 @@ namespace TestVisualCode;
 
 public interface Idb
 {
-    SqliteConnection Connection { get; set; }
+    SqliteConnection? Connection { get; set; }
 
     IEnumerable<T> Read<T>(string sql, Func<T> fu, IEnumerable<SqliteParameter> sqliteParameters);
-    int Read(string sql, Func<int> fu, IEnumerable<SqliteParameter> sqliteParameters);
+    int Read(string sql,  IEnumerable<SqliteParameter> sqliteParameters);
     int Write(string sql, Func<int> fu, IEnumerable<SqliteParameter> sqliteParameters);
 }
